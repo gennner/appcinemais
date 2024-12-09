472390fe83d2c6345d582d5c8201865eda671f86
gennner <33989563+gennner@users.noreply.github.com>
2024-12-08 16:26:22 -0800

    Add files via upload

---
+++ atividade2/ProjetoFilmes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProjetoFilmes
+//
+//  Created by Genner Ramos Maia on 12/3/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
